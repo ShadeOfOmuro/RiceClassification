696f978f3ab13d0209e3272968c5b2858d7cdca5
ShadeOfOmuro <shironi.studio@gmail.com>
2023-01-25 10:11:35 +0700

    fix minor fix

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from fastai.vision.all import *
 app = Flask(__name__)
 print("loading model")
 model = load_learner("bestFull.pth",cpu=True)
-ACCESS_TOKEN = "<YOUR_ACCESS_TOKEN>"
+ACCESS_TOKEN = "F77aAQiczRnjFDAoaahcP352HXsf3mI92O+1zpHpFe+P/1jbZiN34DGpx62GZDYxeJ/aMNIF8BxVSm70tktkiI82Agjfqs+u3BiHLagq7QqNv0yD9WYa03VA01H2wqg781vtdSt2ibxbfxzBLTc5yQdB04t89/1O/w1cDnyilFU="
 line_bot_api = LineBotApi(ACCESS_TOKEN)
-handler = WebhookHandler('<YOUR_CHANNAL_SECRET>')
+handler = WebhookHandler('3c0352a060105077c4266862efa1e4a6')
 
 @app.route("/")
 def home():
@@ -47,12 +47,13 @@ def handle_image(event):
     name = pred[0]
     index = pred[1]
     classes = pred[2]
-    response = """พบว่าเป็นโรค {} ({}%)""".format(name,round(float(classes[index]),2))
-    if name == 'โรค1' :
+    response = """พบว่าเป็นโรค {} ({}%)""".format(name,round(float(classes[index]),4)*100)
+
+    if name == 'tungro' :
       response +="\n วิธีการรักษา"
-    elif name == 'โรค2' :
+    elif name == 'ustilaginoidea' :
       response +="\n วิธีการรักษา"
-    elif name == 'โรค3' :
+    elif name == 'bipolaris' :
       response +="\n วิธีการรักษา"
     else :
       response +="\n วิธีการรักษา"
